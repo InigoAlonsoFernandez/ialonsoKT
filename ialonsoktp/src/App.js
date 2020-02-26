e6647abad4889e0fe07f8cffebda65f26ab9dae4
Iñigo <inigoalonsofernandez@gmail.com>
2020-02-26 16:46:08 +0100

    some changes to see if source tree has been properly connected to github

--- ialonsoktp/src/App.js
+++ ialonsoktp/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'react-bootstrap';
+
+//import Button from 'react-bootstrap/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="headerBar">
+          <span>Username: @Iñigo</span>
+          <span>change user</span>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
